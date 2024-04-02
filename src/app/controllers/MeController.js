2dd099c97e786c129c38a51c69a4dae861eb78c1
Xuanphuc <120350759+Phuc1206@users.noreply.github.com>
2024-04-02 22:42:46 +0700

    count deleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,12 +2,12 @@
 const Course = require('../models/Course');
 class MeController {
   storedCourses(req, res, next) {
-    Course.find({})
-    .then(courses => {
-      courses = courses.map(courses => courses.toObject())
-      res.render('me/stored-courses',{courses: courses});
-    })
-  .catch(next)
+    Promise.all([Course.find({}),Course.countDocumentsWithDeleted({deleted:true})])
+    .then(([courses, deleteCount]) =>{
+        courses = courses.map(courses => courses.toObject());
+        res.render('me/stored-courses',{courses: courses,deleteCount});
+  })
+    .catch(next)
   }
   trash(req,res,next){
     Course.findWithDeleted({deleted: true})
