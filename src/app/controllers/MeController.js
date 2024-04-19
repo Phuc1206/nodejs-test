e094d341683c971cca938e7d9ebffbc8e86c6083
Xuanphuc <120350759+Phuc1206@users.noreply.github.com>
2024-04-20 00:27:24 +0700

    sortable for clean code

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,9 @@ class MeController {
   storedCourses(req, res, next) {
     let CourseQuery = Course.find({});
     
-    if(req.query.hasOwnProperty('_sort')){
-      CourseQuery = CourseQuery.sort({
-        [req.query.column] : req.query.type
-      })
-    }
     
-    Promise.all([CourseQuery,Course.countDocumentsWithDeleted({deleted:true})])
+    
+    Promise.all([Course.find({}).sortable(req),Course.countDocumentsWithDeleted({deleted:true})])
     .then(([courses, deleteCount]) =>{
         courses = courses.map(courses => courses.toObject());
         res.render('me/stored-courses',{courses: courses,deleteCount});
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const mongooseDelete = require('mongoose-delete');
 const slug = require('mongoose-slug-updater');
 
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
@@ -13,7 +14,17 @@ const Course = new Schema({
     slug: { type: String, slug: "name",  unique: true },
 },{
     timestamps:true,
+   
 })
+Course.query.sortable =  function(req){
+    if(req.query.hasOwnProperty('_sort')){
+        const isValidType = ['desc', 'asc'].includes(req.query.type) 
+        return this.sort({
+          [req.query.column] : isValidType ? req.query.type : 'desc'
+        })
+      }
+      return this;
+}
 Course.plugin(mongooseDelete,{
     overrideMethods: 'all',
     deletedAt: true,
